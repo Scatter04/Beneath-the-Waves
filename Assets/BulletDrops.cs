c78f3d7bbb4633a48dd25bb6361439e2052a1fd6
KishanM123 <163074414+KishanM123@users.noreply.github.com>
2024-05-02 12:22:10 +1200

    Add files via upload

---
+++ Assets/BulletDrops.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletDrops : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            PlayerAccount.AddBullets(20);  
+            Destroy(gameObject);  
+        }
+    }
+}
